779aa278f947578717122a4aa5902d7c2e6be205
sharitashya <haritashya@gmail.com>
2019-07-06 18:59:06 +0530

    Fundamentals of JS #D1

--- Introduction_to_JS/Variables/script.js
+++ Introduction_to_JS/Variables/script.js
@@ -1,3 +1,4 @@
+//variables
 var firstName = 'John';
 console.log(firstName);
 
@@ -9,3 +10,48 @@ console.log(fullAge);
 
 job = 'Teacher';
 console.log(job);
+
+
+
+/*Variable mutation and type coercion*/
+//type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight'; //JS dynamic typing
+job = 'driver';
+
+
+
+/* ******************************Operators in JS******************************** */
+// Math 
+now = 24;
+console.log(now - 4);
+
+// Logical
+then = 35;
+var greater = now < then;
+
+// typeof
+console.log(typeof now);
+
+// ------------------------ Study operator precedence on mozilla ------------------------
+// Implementing Ternary operator
+
+var name = 'John';
+var age = 16;
+
+age >= 18 ? console.log(name + ' drinks beer.') : console.log(name + ' drinks juice.');
+
+
+
+
+
+
+
